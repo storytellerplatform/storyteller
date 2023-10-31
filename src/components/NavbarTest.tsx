5d179767f78af3256d10400f304849bcdf3a3c2d
onandon <047633597q@gmail.com>
2023-10-31 19:21:32 +0800

    完成 navbar

--- src/components/NavbarTest.tsx
+++ src/components/NavbarTest.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { FiLogIn, FiMusic } from 'react-icons/fi'
 import { AiOutlineRead } from 'react-icons/ai'
 import { BsCollection } from 'react-icons/bs'
@@ -7,6 +7,19 @@ import classNames from 'classnames';
 
 const NavbarTest = () => {
   const [isHovered, setIsHovered] = React.useState(false);
+
+  const createNavLink = (name: string, nameWidth: string, lineWidth: string, isPresent: boolean, icon: ReactNode) => {
+    return <>
+      <span
+        className={classNames(`relative cursor-pointer
+            before:content-[""] before:absolute before:w-0 ${lineWidth} before:h-[1.5px] before:bottom-0 before:left-10 before:bg-white before:z-50 before:transition-all  before:duration-300 before:ease-out-in 
+            ${name} ${nameWidth} after:absolute after:top-0 after:left-10 after:text-white after:font-bold after:text-sm after:tracking-widest after:z-50 after:opacity-0  after:transition-all after:ease-in-out after:duration-150`,
+          { 'after:opacity-100': isPresent })}
+      >
+        {icon}
+      </span>
+    </>
+  }
 
   return (
     <>
@@ -20,13 +33,44 @@ const NavbarTest = () => {
         </Link>
 
         <nav className='flex flex-col gap-6'>
-          <FiLogIn color='white' size={24} className='cursor-pointer ' />
-          <FiMusic color='white' size={24} className='cursor-pointer' />
-          <BsCollection color='white' size={24} />
+          {createNavLink(
+            "after:content-['登入']",
+            "after:w-10",
+            "hover:before:w-9",
+            isHovered,
+            <FiLogIn
+              color='white'
+              size={24}
+            />
+          )}
+
+          {createNavLink(
+            "after:content-['創作音樂']",
+            "after:w-20",
+            "hover:before:w-16",
+            isHovered,
+            <FiMusic
+              color='white'
+              size={24}
+              className='cursor-pointer'
+            />
+          )}
+
+          {createNavLink(
+            "after:content-['收藏']",
+            "after:w-10",
+            "hover:before:w-9",
+            isHovered,
+            <BsCollection
+              color='white'
+              size={24}
+            />
+          )}
+
         </nav>
 
 
-        <div className={classNames('absolute left-full top-0 h-screen items-center bg-[#191d24] z-50 transition-all duration-150 ease-out-in',
+        <div className={classNames('absolute left-full top-0 h-screen items-center bg-[#191d24] z-40 transition-all duration-150 ease-out-in',
           { 'w-32': isHovered },
           { 'w-0': !isHovered })}
         >
--- src/pages/Home/testPage.tsx
+++ src/pages/Home/testPage.tsx
@@ -4,13 +4,13 @@ import Book from '../../assets/homePageImage.svg'
 
 const TestHome = () => {
   return (
-    <main className='relative px-8 flex flex-row justify-evenly w-full bg-gradient-to-r from-zinc-950 via-zinc-900 via-50% to-zinc-500 text-white'>
+    <main className='relative px-8 flex flex-row justify-evenly w-full bg-gradient-to-r from-zinc-950 via-zinc-900 via-50% to-zinc-700 text-white'>
 
       {/* 標題 */}
       <div className='flex flex-col gap-12 mt-28'>
         <h2 className='text-4xl font-bold'> 說書人 </h2>
         <h1 className='text-6xl font-bold' > 讓你的文字演奏旋律 </h1>
-        <h3 className='text-2xl font-semibold'> 將你的故事、詩或任何文字轉換為美妙的音樂 </h3>
+        <h3 className='mb-8 text-2xl font-semibold'> 將你的故事、詩或任何文字轉換為美妙的音樂 </h3>
 
         <div className='flex gap-6'>
           <button
