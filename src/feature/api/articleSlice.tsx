b3ab0fd7242033379d45777c524d93030a97f0c6
onandon <047633597q@gmail.com>
2024-04-15 21:21:17 +0800

    fix article slice api bug

--- src/feature/api/articleSlice.tsx
+++ src/feature/api/articleSlice.tsx
@@ -35,6 +35,7 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
       query: (articleId) => ({
         url: `/article/${articleId}`,
         method: 'GET',
+        timeout: 10000
       }),
       transformResponse: (response: BArticle): Article => {
         const { articleId, name, content, emotions, createdDate, newestAudioId, allAudioIds } = response;
